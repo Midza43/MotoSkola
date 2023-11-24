1a9cf1c1264fe0a61bfac7c4080ffe278660e090
Elmin Midzic <emidza43@gmail.com>
2023-11-24 18:53:44 +0100

    Registracija, login i početna stranica

---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+    <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/2094647/settings_images/hDtZb1JqR2iljlApr90V_Clutch_Logo_without_Background.png" width="200px" height="200px"></img>
+</a>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,73 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Početna stranica') }}
+        </h2>
+    </x-slot>
+
+    <!-- Container for demo purpose -->
+<div class="container my-24 mx-auto md:px-6">
+  <!-- Section: Design Block -->
+  <section class="mb-32 text-center lg:text-left">
+    <div
+      class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] white">
+      <div class="flex flex-wrap items-center">
+        <div class="block w-full shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Army_National_Guard_motorcycle_riders.jpg" alt="Pocetna-Slika"
+            class="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg" />
+        </div>
+        <div class="w-full shrink-0 grow-0 basis-auto lg:w-6/12 xl:w-8/12">
+          <div class="px-6 py-12 md:px-12">
+            <h2 class="display-5 mb-6 text-4xl font-bold text-primary dark:text-primary-400">
+              MOTO ŠKOLA
+            </h2>
+            <p class="mb-12 text-neutral-500">
+            Dobrodošli u našu moto školu, gdje se strast prema vožnji susreće s iskusnim instruktorima i vrhunskom opremom. Naša moto škola nije samo mjesto gdje ćete naučiti osnove za dobivanje vozačke dozvole, već i iskustvo koje će vam pružiti sigurnost i povjerenje na cesti.
+
+Naši instruktori nisu samo stručnjaci u vožnji, već su i zaljubljenici u motore koji dijele istu strast kao i vi. Njihov cilj je ne samo prenijeti vam potrebna znanja i vještine, već i inspirirati vas da razvijete ljubav prema vožnji koja će trajati cijeli život.
+
+S bogatim izborom motora prilagođenim vašim potrebama i razinama iskustva, omogućujemo vam da se osjećate udobno na putu. Sigurnost nam je najvažnija, pa se naši motori redovito održavaju i opremljeni su najnovijom tehnologijom kako bi vam pružili potrebnu zaštitu.
+
+Kvaliteta obuke je ključna odrednica naše moto škole. Naš program obuke obuhvaća sve aspekte vožnje, uključujući teorijsko obrazovanje, vježbe na poligonu i praktičnu vožnju u stvarnom prometu. Osim toga, nudimo i dodatne tečajeve za napredne vozače kako bismo vam pomogli stalno unaprjeđivati svoje vještine i ostati sigurni na cesti.
+
+Pridružite nam se i doživite uzbuđenje vožnje na pravi način, uz podršku stručnih instruktora, visokokvalitetne opreme i raznovrsni izbor motora. Vaša avantura na dva točka počinje ovdje!
+            </p>
+
+            <div class="grid md:grid-cols-3 lg:gap-x-12">
+              <div class="mb-12 md:mb-0">
+                <h2 class="mb-4 text-3xl font-bold text-primary dark:text-blue-900">
+                  7456
+                </h2>
+                <h5 class="mb-0 text-lg font-medium">
+                  Zadovoljnih kandidata
+                </h5>
+              </div>
+
+              <div class="mb-12 md:mb-0">
+                <h2 class="mb-4 text-3xl font-bold text-primary dark:text-blue-900">
+                  Širok izbor motora
+                </h2>
+                <h5 class="mb-0 text-lg font-medium">
+                  Posjedujemo motore za sve kategorije
+                </h5>
+              </div>
+
+              <div class="">
+                <h2 class="mb-4 text-3xl font-bold text-primary dark:text-blue-900">
+                  12
+                </h2>
+                <h5 class="mb-0 text-lg font-medium">
+                  Instruktora za obuku
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Section: Design Block -->
+</div>
+<!-- Container for demo purpose -->
+    
+</x-app-layout>
